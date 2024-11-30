1c3c7966c07a04112b92b86d07a8f0bf9b8e2094
Rushabh sutariya <sutariyarushabh2@gmail.com>
2024-11-30 17:02:48 +0530

    TipTap Extension

--- src/app/documents/[documentsId]/editor.tsx
+++ src/app/documents/[documentsId]/editor.tsx
@@ -1,4 +1,12 @@
 "use client";
+import Image from "@tiptap/extension-image";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import TaskItem from "@tiptap/extension-task-item";
+import ImageResize from "tiptap-extension-resize-image";
+import TaskList from "@tiptap/extension-task-list";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
@@ -11,8 +19,25 @@ export const Editor = () => {
           "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      StarterKit,
+      Image,
+      ImageResize,
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+    ],
+    content: `
+    <img src="https://i.pinimg.com/736x/96/1d/6d/961d6d5199d11056b10eea2cf8cd3c5b.jpg" />
+
+  `,
   });
   return (
     <div className="size-full overflow-x-auto bg-[#F9FDFB] px-4 print:bg-white print:p-0 print:overflow-visible">
